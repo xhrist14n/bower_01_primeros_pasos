ddfc16bfb8af7df37b271c2569b35a45f9b4de3f
vagrant <vagrant@precise32.(none)>
2016-01-11 20:00:13 +0000

    bower y explicacion

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bower_01_primeros_pasos
 primeros pasos en bower 
+
+# Instrucciones deben usar los comandos para ejecutar el proyecto
+
+npm install 
+bower install
+
+#video del proyecto con explicacion de bower
+
+https://youtu.be/wRzhdUvMX1s
+
+
---
+++ bower_run.sh
@@ -0,0 +1,14 @@
+bower init
+
+bower search modernizr
+bower search jquery
+bower search respond
+bower search html5shiv
+bower search font-awesome
+
+bower install jquery --save
+bower install modernizr --save
+bower install respondJS --save
+bower install html5shiv --save
+bower install font-awesome --save
+
